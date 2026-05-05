71a5b328479fa483231a93fd311a2af1609bacde
Basma <kassimibasma17@gmail.com>
2026-05-05 23:33:35 +0200

    Ajout de app et nav bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'inventory_screen.dart';
+import 'barcode_scan_screen.dart';
+import 'recipe_results_screen.dart';
+import 'shopping_list_screen.dart';
+
+import '../widgets/custom_app_bar.dart';
+import '../widgets/custom_bottom_nav_bar.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+
+ // home_screen.dart
+
+final List<Widget> pages = const [
+  Center(child: Text("Welcome Home Page")),
+  InventoryPage(), 
+ ScanPage(),
+  RecipesPage(),
+ ListPage(),
+];
+
+  void onTabTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Hello Welcome to H O M E"),
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: IndexedStack(
+        index: currentIndex,
+        children: pages,
+      ),
+      bottomNavigationBar: CustomBottomNav(
+        currentIndex: currentIndex,
+        onTap: onTabTapped,
       ),
     );
   }
---
+++ lib/screens/inventory_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class InventoryPage extends StatelessWidget {
+  const InventoryPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("📦 Inventory Page"));
+  }
+}
